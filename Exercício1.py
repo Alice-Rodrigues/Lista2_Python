0d2602010e5c4843cba9be970f7d57452d1c1de4
Alice00101101Rodrigues <54628916+Alice-Rodrigues@users.noreply.github.com>
2022-04-25 09:59:59 -0400

    Update and rename Exercício1.py to Exercício02.py

--- Exercício1.py
+++
@@ -1,9 +0,0 @@
-
-#Faça um Programa que peça dois números e imprima o maior deles.
-
-nnumero1 = float(input("Digite um número: "))
-numero2 = float(input("Digite outro número: "))
-
-if numero1 > numero2: print("O maior número é ", numero1)
-elif numero2 > numero1: print("O maior número é ", numero2)
-else: print("Os números são iguais")
